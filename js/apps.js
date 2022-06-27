fe0622485d92be793d97646130916f2ea91a258d
Camille <cam.duplessis@gmail.com>
2022-06-27 15:37:36 +1000

    Include current time & date

--- js/apps.js
+++ js/apps.js
@@ -1,15 +1,39 @@
-//set-up API integration for changing data
+//Show time & date
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
 
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
+//set-up API integration for changing data
 function showTemp(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
   let windElement = document.querySelector("#wind");
-
+  let humidityElement = document.querySelector("#humidity");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   windElement.innerHTML = Math.round(response.data.wind.speed);
+  humidityElement.innerHTML = response.data.main.humidity;
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apikey = "cafbd7974d35bf96f37717d1116fdc4b";
